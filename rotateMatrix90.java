14fa6b78160668e7b6cc1e8be64cff1e494f91e7
garvapg782 <garv.anand187@gmail.com>
2025-09-16 21:49:07 +0530

    Update rotateMatrix90.java

--- rotateMatrix90.java
+++ rotateMatrix90.java
@@ -2,7 +2,7 @@ package Class;
 
 import java.util.Scanner;
 
-public class RotateMatrix90 {
+public class rotateMatrix90 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
