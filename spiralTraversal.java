418953266af84429f774c64972703529ff5d8112
garvapg782 <garv.anand187@gmail.com>
2025-09-16 22:31:43 +0530

    spiralTraversal.java

--- spiralTraversal.java
+++ spiralTraversal.java
@@ -1,37 +1,38 @@
-import java.util.Scanner;
-public class spiralTraversal{
-        public static void main(String[] args) {
-            Scanner sc = new Scanner(System.in);
+import java.util.*;
 
-            int rows = sc.nextInt();
-            int cols = sc.nextInt();
-            int[][] matrix = new int[rows][cols];
-
-            for (int i = 0; i < rows; i++)
-                for (int j = 0; j < cols; j++)
-                    matrix[i][j] = sc.nextInt();
-
-            int top = 0, bottom = rows - 1;
-            int left = 0, right = cols - 1;
-
-
-            while (top <= bottom && left <= right) {
-                for (int j = left; j <= right; j++) System.out.print(matrix[top][j] + " ");
-                top++;
-
-                for (int i = top; i <= bottom; i++) System.out.print(matrix[i][right] + " ");
-                right--;
-
-                if (top <= bottom) {
-                    for (int j = right; j >= left; j--) System.out.print(matrix[bottom][j] + " ");
-                    bottom--;
+class Solution {
+    public List<Integer> spiralOrder(int[][] matrix) {
+        List<Integer> result = new ArrayList<>();
+        
+        if (matrix.length == 0) return result;
+        
+        int top = 0, bottom = matrix.length - 1;
+        int left = 0, right = matrix[0].length - 1;
+        
+        while (top <= bottom && left <= right) {
+            for (int j = left; j <= right; j++) {
+                result.add(matrix[top][j]);
+            }
+            top++;
+            for (int i = top; i <= bottom; i++) {
+                result.add(matrix[i][right]);
+            }
+            right--;
+            
+            if (top <= bottom) {
+                for (int j = right; j >= left; j--) {
+                    result.add(matrix[bottom][j]);
                 }
-
-                if (left <= right) {
-                    for (int i = bottom; i >= top; i--) System.out.print(matrix[i][left] + " ");
-                    left++;
+                bottom--;
+            }
+            if (left <= right) {
+                for (int i = bottom; i >= top; i--) {
+                    result.add(matrix[i][left]);
                 }
+                left++;
             }
         }
+        
+        return result;
     }
-
+}
